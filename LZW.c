8f2398ca225053d5a7532b2085593a1c6f67249b
Gosewinckel <gosewinckel.lewis@gmail.com>
2025-06-10 07:57:30 +1000

    not taking an hour but still not working properly

--- LZW.c
+++ LZW.c
@@ -15,7 +15,7 @@ struct dict {
 	size_t seq_len;
 }; typedef struct dict dict_t;
 
-uint32_t hash(uint8_t seq[], size_t seq_len) {
+uint64_t hash(uint8_t seq[], size_t seq_len) {
 	uint64_t hash = 14695981039346656037u; 
 	for(int i = 0; i < seq_len; i++) {
 		hash ^= seq[i];
@@ -37,6 +37,7 @@ bool check_seq(uint8_t seq[], size_t seq_len, dict_t dict[], uint64_t key) {
 }
 
 //return index to next free dictionary element of index of existing sequence
+//check_sequence will return true if sequences are identical i.e. idx is the existing val
 int search(uint64_t key,  dict_t dict[], size_t dict_size, uint8_t seq[], size_t seq_len, bool *check_sequence) {
 	key = key % dict_size;
 	for(uint64_t i = key; i < dict_size; i++) {
@@ -46,7 +47,7 @@ int search(uint64_t key,  dict_t dict[], size_t dict_size, uint8_t seq[], size_t
 		}
 		*check_sequence = check_seq(seq, seq_len, dict, key);
 		//if sequence is correct return the index
-		if(check_sequence) {
+		if(*check_sequence == true) {
 			return i;
 		}
 	}
@@ -86,7 +87,9 @@ void insert(dict_t dict[], uint64_t key, uint8_t sequence[], size_t seq_len, int
 
 void free_dict(dict_t dict[], size_t dict_size) {
 	for(int i = 0; i < dict_size; i++) {
-		free(dict[i].sequence);
+		if(dict[i].sequence != NULL) {
+			free(dict[i].sequence);
+		}
 	}
 	free(dict);
 	return;
@@ -139,8 +142,111 @@ const char *LZW(FILE *file, char *name) {
 		int idx = search(key, dict, *dict_size, init_seq, 1,  check_sequence);
 		insert(dict, key, init_seq, 1, idx);
 	}
+	
+	//loop through image 3 times to find red green and blue values consecutively
+	//find longest sequence and save dictionary index to output
+	uint64_t *output = malloc(sizeof(uint64_t) * pixels/2);
+	size_t out_len = 0;
+	uint8_t *next_seq = malloc(sizeof(uint64_t) * pixels/2);
+	size_t seq_len = 0;
+	int last_known_idx = 0;
+	//red vals
+	for(int i = 0; i < height; i++) {
+		for(int j = 0; j < width; j++) {
+			uint8_t current = image[i][j].rgbtRed;
+			next_seq[seq_len] = current;
+			seq_len++;
+			uint64_t next_key = hash(next_seq, seq_len);
+			bool known = false;
+			bool *is_known_seq = &known;
+			int idx = search(next_key, dict, *dict_size, next_seq, seq_len, is_known_seq);
 
+			//if -1 something went wrong
+			if(idx == -1) {
+				printf("something went wrong\n");
+				return NULL;
+			}
+			//new dict entry
+			if(*is_known_seq == false) {
+				seq_len = 0;
+				last_known_idx = idx;
+			}
+			//is known sequence
+			if(*is_known_seq) {
+				output[out_len] = last_known_idx;
+				out_len++;
+				uint8_t *new_seq = malloc(sizeof(uint8_t) * seq_len);
+				for(int x = 0; x < seq_len; x++) {
+					new_seq[x] = next_seq[x];
+				}
+				insert(dict, next_key, next_seq, seq_len, idx);
+			}
+		}
+	}
 
-	free_dict(dict, *dict_size);
+	//green vals
+	for(int i = 0; i < height; i++) {
+		for(int j = 0; j < width; j++) {
+			uint8_t current = image[i][j].rgbtGreen;
+			next_seq[seq_len] = current;
+			seq_len++;
+			uint64_t next_key = hash(next_seq, seq_len);
+			bool known = false;
+			bool *is_known_seq = &known;
+			int idx = search(next_key, dict, *dict_size, next_seq, seq_len, is_known_seq);
+
+			//if -1 something went wrong
+			if(idx == -1) {
+				printf("something went wrong\n");
+				return NULL;
+			}
+			//new dict entry
+			if(*is_known_seq == false) {
+				seq_len = 0;
+				last_known_idx = idx;
+			}
+			//is known sequence
+			if(*is_known_seq) {
+				output[out_len] = last_known_idx;
+				out_len++;
+				uint8_t *new_seq = malloc(sizeof(uint8_t) * seq_len);
+				for(int x = 0; x < seq_len; x++) {
+					new_seq[x] = next_seq[x];
+				}
+				insert(dict, next_key, next_seq, seq_len, idx);
+			}
+		}
+	}
+
+	//blue vals
+	for(int i = 0; i < height; i++) {
+		for(int j = 0; j < width; j++) {
+			uint8_t current = image[i][j].rgbtBlue;
+			next_seq[seq_len] = current;
+			seq_len++;
+			uint64_t next_key = hash(next_seq, seq_len);
+			bool known = false;
+			bool *is_known_seq = &known;
+			int idx = search(next_key, dict, *dict_size, next_seq, seq_len, is_known_seq);
+
+			//if -1 something went wrong
+			if(idx == -1) {
+				printf("something went wrong\n");
+				return NULL;
+			}
+			//unknown sequence
+			if(*is_known_seq == false) {
+
+			}
+			//is known sequence
+			if(*is_known_seq) {
+
+			}
+		}
+	}
+
+	free(output);
+	free(next_seq);
+	//free_dict(dict, *dict_size);
 	return c_file_name;
 }
